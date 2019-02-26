7b4ebe61959caeefbe3bd1f7b6a5a246f5e6e196
weichengzhong <weichengzhong@hk01.com>
2019-02-26 12:17:53 +0800

    feat: 添加单元测试

---
+++ tests/IndexTest.php
@@ -0,0 +1,17 @@
+<?php
+namespace tests;
+use PHPUnit\Framework\TestCase;
+
+
+class IndexTest extends TestCase
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function testIndex()
+    {
+        $this->assertEquals(0, 0);
+    }
+}
